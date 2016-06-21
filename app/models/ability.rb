0b12ac6fab907c1cb841a06c6aecfdbd6e6251f2
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-06-21 18:20:06 -0500

    make user as admin for role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,13 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
+    if user.admin? :admin
+        can :manage, :all
+    else
+        can :read, :all
+    end
+
+
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ActiveRecord::Base
+	resourcify
+	
 	validates :title, presence: true
 	validates :company, presence: true
 	validates :summary, presence: true
