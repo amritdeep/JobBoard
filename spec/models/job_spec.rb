5dd58a544844d71ffb57e1928527da0bf97e19a6
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-06-21 12:19:44 -0500

    test validation of job model

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
   it "should have title, location and summary" do
-  	job = build(:job)
-  	binding.pry
-  	expect(:job).to_be valid
+  	job = create(:job)
+  	expect(job.title).to eql("ROR Developer")
+  	expect(job.location).to eql("TX")
+  	expect(job.summary).to eql("ROR Developer")
+  	expect(job.company).to eql("My company")
   end
 
 end
