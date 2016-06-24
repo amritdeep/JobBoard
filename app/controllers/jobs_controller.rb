817bf2d71103e073393e1a530b941088dd0e4990
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-06-23 22:21:50 -0500

    Company user can post job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,32 @@ class JobsController < ApplicationController
   	@job = Job.all
   end
 
-  def new	
+  def new
+  	if current_user.role == "company"
+  		@job = current_user.jobs.new
+  	else
+  		flash[:notice] = "You are not admin or company user"
+  		render :index
+  	end
+  end
+
+  def create
+  	@job = current_user.jobs.build(job_param)
+  	if @job.save
+  		redirect_to action: "show"	, id: @job.id
+  	else
+  		flash[:notice] = "Require field"
+  		render :new
+  	end
+  end
+
+  def show
+  end
+
+  private
+
+  def job_param
+  	params.require(:job).permit(:title, :location, :company, :summary)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621221649) do
+ActiveRecord::Schema.define(version: 20160624021523) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -22,16 +22,13 @@ ActiveRecord::Schema.define(version: 20160621221649) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "resource_id"
-    t.string   "resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "jobs_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "job_id",  null: false
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-  add_index "roles", ["name"], name: "index_roles_on_name"
+  add_index "jobs_users", ["job_id", "user_id"], name: "index_jobs_users_on_job_id_and_user_id"
+  add_index "jobs_users", ["user_id", "job_id"], name: "index_jobs_users_on_user_id_and_job_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,16 +43,10 @@ ActiveRecord::Schema.define(version: 20160621221649) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-  end
-
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
 end
