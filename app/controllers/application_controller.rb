d64f9cc02d1c2b6d80623afd30a411414ac54a68
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-06-23 22:18:25 -0500

    User define their roles while registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include CanCan::ControllerAdditions
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :role) } 
+	end  
+
 end
