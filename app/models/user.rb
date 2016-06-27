e954ab4564ad7f16e4ddb4649dafca4ae5ad75be
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2016-06-27 13:59:53 -0500

    show relationship between job and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ActiveRecord::Base
   # rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, #:registerable,
+         :recoverable, :rememberable, :trackable #, :validatable
 
   ROLES = %w[admin company employee employer].freeze 
 
-  has_and_belongs_to_many :jobs      
+  validates :email, uniqueness: false, presence: true
+  validates :password, presence: false
+
+  has_and_belongs_to_many :jobs 
 end
